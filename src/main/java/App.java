422f397e680d7f48fef654969995550d3bc16a42
anaboru <slysetrrr71969@gmail.com>
2022-11-18 23:21:34 +0300

    5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorldBean.getMessage());
+        HelloWorld helloWorldBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBean == helloWorldBean2);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean == catBean2);
+
     }
 }
